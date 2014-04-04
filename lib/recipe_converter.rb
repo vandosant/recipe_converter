e5ff1f188fc1ddf421dfb9cc236a0f1433978a2c
vandosant <sjskender@gmail.com>
2014-04-04 09:27:28 -0600

    Can convert the ingredients to tablespoons

--- lib/recipe_converter.rb
+++ lib/recipe_converter.rb
@@ -1,7 +1,16 @@
 class RecipeConverter
   attr_reader :ingredients
-  
+
   def initialize(ingredients)
     @ingredients = ingredients
+  end
+
+  def to_tablespoons
+    converted_ingredients = Array.new
+    @ingredients.each do |ingredient, amount|
+      tablespoons = (amount.to_f/15).round(2)
+      converted_ingredients << {ingredient => tablespoons}
+    end
+    converted_ingredients
   end
 end
--- spec/recipe_converter_spec.rb
+++ spec/recipe_converter_spec.rb
@@ -1,10 +1,18 @@
 require 'recipe_converter'
 
 describe RecipeConverter do
-  it "holds a list of ingredients and amounts" do
+  it "holds a list of ingredients and amounts in grams" do
     test_recipe = RecipeConverter.new(:sugar => 100, :flour => 400, :butter => 35)
     actual = test_recipe.ingredients
     expected = {:sugar => 100, :flour => 400, :butter => 35}
+
+    expect(actual).to eq expected
+  end
+
+  it "converts the amount of each ingredient to tablespoons" do
+    test_recipe = RecipeConverter.new(:sugar => 100, :flour => 400, :butter => 35)
+    actual = test_recipe.to_tablespoons
+    expected = [{:sugar => 6.67}, {:flour => 26.67}, {:butter => 2.33}]
 
     expect(actual).to eq expected
   end
